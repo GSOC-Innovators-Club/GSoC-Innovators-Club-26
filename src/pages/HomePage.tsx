e1b1ee6749266c92940d8b83d000c6a950f9fe6c
Jyotibrat <jyotibrat@gmail.com>
2026-06-26 02:36:27 +0530

    feat[sponsors]: added the sponsors section in the home Pg.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from '../components/HeroSection/HeroSection';
+import { SponsorsSection } from '../components/SponsorsSection/SponsorsSection';
 import { EventsSection } from '../components/EventsSection/EventsSection';
 
 export function HomePage() {
     return (
         <main>
             <HeroSection />
+            <SponsorsSection />
             <EventsSection />
         </main>
     );
